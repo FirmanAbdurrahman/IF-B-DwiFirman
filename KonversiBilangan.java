a3fcf978e7c5d3eb13f9723a44bfd4b4d387f608
unknown <ichsanmuiz@gmail.com>
2014-09-26 15:49:27 +0700

    Tugas TIM

---
+++ KonversiBilangan.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class KonversiBilangan {
+		static String name; 
+			static int bilDes;
+		public static void Biner (int n){
+			if(n>1){
+				Biner(n/2);
+			}
+				System.out.print(n%2);
+		}
+			public static void Hexa (int n){
+				char[] daftarHexa={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+					if(n>1){
+						Hexa(n/16);
+					}
+							System.out.print(daftarHexa[n%16]);
+      
+			}
+			public static void Oktal (int n){
+				char[] daftarOktal={'0','1','2','3','4','5','6','7','8'};
+					if(n>1){
+						Oktal (n/8);
+					}
+				System.out.print(daftarOktal[n%8]);
+			}
+			public static void inputDes(){
+					System.out.print("Masukkan bilangan desimal = ");
+						Scanner inputan=new Scanner(System.in);
+							bilDes=inputan.nextInt();
+			}
+				public static void tampilBiner(){
+					inputDes();
+						System.out.print("\nBinernya adalah ");	
+							Biner(bilDes);
+				}
+					public static void tampilHexa(){
+						inputDes();
+							System.out.print("\nHexanya adalah  ");
+								Hexa(bilDes);
+					}
+		public static void tampilOktal(){
+			inputDes();
+				System.out.print("\nOktalnya adalah ");
+					Oktal(bilDes);
+		}
+			public static int inpBil(){
+							System.out.println("Silahkan Memilih Menu Konversi Bilangan ke: " + "\n1. Biner  \n2. Hexa \n3. Oktal \n4. Exit");
+							System.out.print("\n-------------------------------------");
+							System.out.print("\nMasukkan Angka Dari Menu Di Atas = ");
+									Scanner inputan=new Scanner (System.in);
+						int pilmenu=inputan.nextInt();
+					return pilmenu;
+			}
+  
+		public static void menu (int pil){
+			System.out.println(" ");
+				switch(pil){
+					case 1: tampilBiner(); break;
+					case 2: tampilHexa() ; break;
+					case 3: tampilOktal(); break;
+					case 4: System.out.println("Trimakasih");
+						System.exit(0); break;
+							default:System.out.println("(Maaf. Pilih angka 1-4 saja)" + ""); break;
+				}
+				System.out.println(" ");
+		}
+		public static void main(String arg[]){
+				{
+				menu(inpBil());
+				}
+        }
+}
